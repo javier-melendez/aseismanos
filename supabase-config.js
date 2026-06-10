5f4a4ce81c9897dac3a47d50cbf1fd4f2077803c
JAVIER ANDRES MELENDEZ ORDONEZ <122297305+javier-melendez@users.noreply.github.com>
2026-06-10 18:24:04 -0500

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
   supabaseUrl: "https://djssoyfrdgelbiwpxwtl.supabase.co",
-  supabaseAnonKey: "sb_publishable_TdDSsFT36gkHwxhvZTDLOA_VqdqhsD",
+  supabaseAnonKey: "sb_publishable_TdDSsFT36gkHwxhvZTDLOA_VqdqhsDO",
 };
